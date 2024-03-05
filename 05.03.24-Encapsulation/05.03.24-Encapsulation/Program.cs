736379754d3820648689c8761982c159f55b3f1b
alexberg90 <ayazya@code.edu.az>
2024-03-05 20:18:34 +0400

    update

--- 05.03.24-Encapsulation/05.03.24-Encapsulation/Program.cs
+++ 05.03.24-Encapsulation/05.03.24-Encapsulation/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using _05._03._24_Encapsulation;
+
 Console.WriteLine("Hello, World!");
+
+
+User User = new User();
+Console.WriteLine("enter username");
+User.Username = Console.ReadLine();
+
+
+while (User.Password==null)
+{
+Console.WriteLine("enter password");
+User.Password = Console.ReadLine();
+
+}
+while (User.Age>0)
+{
+Console.WriteLine("Yas daxil edin:");
+User.Age =Convert.ToInt32(Console.ReadLine()) ;
+}
+
---
+++ 05.03.24-Encapsulation/05.03.24-Encapsulation/User.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05._03._24_Encapsulation
+{
+    internal class User
+    {
+
+        public string Username { get; set; }
+
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length > 8)
+                {
+                    Console.WriteLine("Duzgun sifre daxil edin");
+                }
+                else if(!Uppercase(value))
+                {
+                    Console.WriteLine("en az bir boyuk herf olmalidi:");
+                }
+
+                else if (!ContainsDigit(value))
+                {
+                    Console.WriteLine("en az bir reqem olmalidi");
+                }
+                else
+                {
+                    _password=value;
+                }
+
+            }
+        }
+
+
+
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+
+                if (value > 0)
+                {
+                    value = _age;
+                }
+                else
+                {
+                    Console.WriteLine("yas menfi ola bilmez");
+                }
+            }
+        }
+
+
+
+        private static bool Uppercase(string text)
+        {
+
+
+            foreach (char c in text)
+            {
+                if (char.IsUpper(c))
+                {
+                    return true;
+                }
+                
+
+            }
+            return false;
+        }
+
+        private static bool ContainsDigit(string text)
+        {
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                {
+                    return true; 
+                }
+
+            }
+            return false; 
+        }
+    }
+}
